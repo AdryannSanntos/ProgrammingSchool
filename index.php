1b5fa98b46ea7f3698de2d0748bdf717f749e262
Adryan Santos <98724000+AdryannSanntos@users.noreply.github.com>
2022-03-29 14:30:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,44 @@
 <?php include('config.php'); ?>
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-  <meta charset='utf-8'>
-  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-  <title>Index</title>
-  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel='stylesheet' type='text/css' media='screen' href='<?php echo INCLUDE_PATH;?>Css/main.css'>
+  <title>ProgrammingSchool</title>
 </head>
 <body>
-  <header>
+  <div class="root">
     <div class="container-header">
-      <div class="container-title">
-        <h1>Programming <br> School</h1>
-      </div>
-      <div class="container-buttons">
-        <nav>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="<?php echo INCLUDE_PATH_PAINEL;?>">Login</a></li>
-          </ul>
-        </nav>
-      </div>
+      <header>
+        <div class="container-title">
+          <h1>Programming</h1>
+          <p>School</p>
+        </div>
+        <div class="container-buttons">
+          <nav>
+            <ul>
+              <a href="#Home">Home</a>
+              <a href="<?php echo INCLUDE_PATH_PAINEL;?>">Login</a>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </div>
-  </header>
+    <div class="container-content">
+      <main>
+        <div class="block left">
+          <img src="./Assets/undraw_code_typing_re_p8b9.svg" alt="">
+        </div>
+        <div class="block right">
+          <h1>Quem somos nós?</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, iure autem doloremque perspiciatis ex culpa? Natus nobis magni perspiciatis ullam nostrum minima quod, dolores laudantium, impedit iure dolorum molestias. Pariatur.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, iure autem doloremque perspiciatis ex culpa? Natus nobis magni perspiciatis ullam nostrum minima quod, dolores laudantium, impedit iure dolorum molestias. Pariatur.</p>
+          <br>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, iure autem doloremque perspiciatis ex culpa? Natus no Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, iure autem doloremque perspiciatis ex culpa? Natus no Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, iure autem doloremque perspiciatis ex culpa? Natus no</p>
+        </div>
+      </main>
+    </div>
+  </div>
 </body>
-</html>
+</html> 
